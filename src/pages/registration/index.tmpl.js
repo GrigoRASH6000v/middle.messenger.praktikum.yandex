0f63ddede7e846d35c61dd3af270e054167aec25
Grigorij <mikigrigorij@yandex.ru>
2021-07-17 10:57:49 +0300

    final commit for branch sprint_1

--- src/pages/registration/index.tmpl.js
+++ src/pages/registration/index.tmpl.js
@@ -1,37 +1,39 @@
 export default `<div class="registration">
-    <div class="form registration__form">
-        <h3 class="form__title">Регистрация</h3>
-        <label for="form_input-login" class="label">
-            Почта
-            <input placeholder="ivanov@mail.ru" id="form_input-login" class="input input--border-botoom" type="text">
-        </label>
-        <label for="form_input-login" class="label">
-            Логин
-            <input placeholder="ivanov@mail.ru" id="form_input-login" class="input input--border-botoom" type="text">
-        </label>
-        <label for="form_input-login" class="label">
-            Имя
-            <input placeholder="ivanov@mail.ru" id="form_input-login" class="input input--border-botoom" type="text">
-        </label>
-        <label for="form_input-login" class="label">
-            Фамилия
-            <input placeholder="ivanov@mail.ru" id="form_input-login" class="input input--border-botoom" type="text">
-        </label>
-        <label for="form_input-login" class="label">
-            Телефон
-            <input placeholder="ivanov@mail.ru" id="form_input-login" class="input input--border-botoom" type="text">
-        </label>
-        <label for="form_input-password" class="label">
-            Пароль
-            <input id="form_input-password" class="input input--border-botoom" type="password">
-        </label>
-        <label for="form_input-password" class="label">
-            Пароль (ещё раз)
-            <input id="form_input-password" class="input input--border-botoom" type="password">
-        </label>
-        <div class="form__buttons">
-            <a class="link-btn" href="/login">Зарегистрироваться</a>
-            <a class="link-btn--text" href="/">Войти</a>
+    <div class="registration__form">
+        <div class="form__container">
+            <h3 class="title-h2">Регистрация</h3>
+            <label class="label">
+                Почта
+                <input placeholder="ivanov@mail.ru" class="input input--border-bottom" type="text">
+            </label>
+            <label class="label">
+                Логин
+                <input placeholder="ivanov@mail.ru" class="input input--border-bottom" type="text">
+            </label>
+            <label class="label">
+                Имя
+                <input placeholder="ivanov@mail.ru" class="input input--border-bottom" type="text">
+            </label>
+            <label class="label">
+                Фамилия
+                <input placeholder="ivanov@mail.ru" class="input input--border-bottom" type="text">
+            </label>
+            <label class="label">
+                Телефон
+                <input placeholder="ivanov@mail.ru" class="input input--border-bottom" type="text">
+            </label>
+            <label class="label">
+                Пароль
+                <input class="input input--border-bottom" type="password">
+            </label>
+            <label class="label">
+                Пароль (ещё раз)
+                <input class="input input--border-bottom" type="password">
+            </label>
+            <div class="form__buttons">
+                <a class="link-btn" href="/login">Зарегистрироваться</a>
+                <a class="link-btn--text" href="/">Войти</a>
+            </div>
         </div>
     </div>
 </div>`;
